834121c8dddf9c0243b9e114ab5e4b12653d3e58
Maxim Vandebroeck <maxim.vandebroeck@gmail.com>
2026-04-17 23:15:10 +0200

    hass-sma-rs 0.1.4: verbose RUST_LOG=debug during bring-up

--- hass-sma-rs/run.sh
+++ hass-sma-rs/run.sh
@@ -6,4 +6,8 @@ set -eu
 bashio::log.info "hass-sma-rs starting"
 bashio::log.info "config: $(cat /data/options.json)"
 
+# Verbose protocol logs during this bring-up phase; reduce once stable.
+export RUST_LOG="${RUST_LOG:-debug,rumqttc=info,hyper=info,sqlx=info}"
+export RUST_BACKTRACE=1
+
 exec /usr/local/bin/hass-sma-daemon --config /data/options.json
